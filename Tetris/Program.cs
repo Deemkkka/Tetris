d44bcb2629e9db298f17366bb97b2103d9fc59f6
Deem <Deem@DEEM>
2022-08-31 16:58:40 +0300

    Drow -> Draw

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -17,13 +17,15 @@ namespace Tetris
             int y1 = 3;
             char c1 = '*';
 
-            Drow(x1, y1, c1);
+            Draw(x1, y1, c1);
 
+
+            //Relay
             Console.ReadKey();
         }
 
 
-        static void Drow(int x, int y, char c)
+        static void Draw(int x, int y, char c)
         {
             Console.SetCursorPosition(x, y);
             Console.Write(c);
